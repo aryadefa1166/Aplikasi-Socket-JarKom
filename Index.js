360089a6795a73db36ade5ac1b7ad04d7dd4aa1a
Nailah <salmahnailah23@gmail.com>
2026-03-04 21:48:42 +0700

    Test 1

---
+++ Index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Memberitahu express untuk mengambil file statis (html/css) dari folder 'public'
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Ada user konek nih!');
+    
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Kirim pesan ke semua orang
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server nyala di http://localhost:3000');
+});
